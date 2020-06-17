b54526250057316d953ef08a34e27f4dd5aa04d6
june <brainyjun@gmail.com>
2020-06-17 18:10:23 +0900

    base url change

--- src/utils/Urls.js
+++ src/utils/Urls.js
@@ -1,6 +1,6 @@
 const urls = {
-    BASE_URL: 'http://192.168.0.32:4000',
-    SOCKET_URL: 'http://192.168.0.32:3005',
+    BASE_URL: 'http://52.78.41.75:4000',
+    SOCKET_URL: 'http://52.78.41.75:3005',
     END_LOAD_PAGE: `/api/loadPage`,
     END_MAX_PAGE: `/api/getMaxPage`,
     END_CHECK_ROOM: `/api/checkRoom`,
